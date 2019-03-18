323233747aadea0be9cf5e779c14f54799d10981
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-03-18 11:42:06 +0000

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const geocode = require("./geocode");
+const forecast = require("./forecast");
+
+const publicDirectory = path.join(__dirname, "../public");
+app.use(express.static(publicDirectory));
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    res.send("Please provide an address");
+  } else {
+    geocode(req.query.address, (error, response) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      forecast(response.latitude, response.longitude, (error, forecastData) => {
+        if (error) {
+          return console.log(error);
+        }
+
+        res.send({
+          forecast: forecastData,
+          location: response.location,
+          address: req.query.address
+        });
+      });
+    });
+  }
+});
+
+app.listen(80, () => {
+  console.log("listening on port 3000");
+});
